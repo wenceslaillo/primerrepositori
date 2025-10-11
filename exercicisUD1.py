3478e1c072dda9b8b50c0a1de7bcd539179e01c9
wenceslaillo <coponcopincopete@hotmail.es>
2025-10-12 00:16:46 +0200

    Add files via upload

---
+++ exercicisUD1.py
@@ -0,0 +1,54 @@
+# exercici 1
+
+print("Exercici 1:\n")
+print("Hola Món\n")
+input("Polsa intro per continuar.")
+
+# exercici 2
+# a)Realitza un programa en Python en el qual es declaren les variables
+# senceres x i y. Assigna'ls els valors 144 i 999 respectivament. A
+# continuació, mostra per pantalla el valor de cada variable,
+# la suma, la resta, la multiplicació, la divisió,
+# la divisió sencera i la resta de la divisió sencera.
+
+x = 144
+y = 999
+
+print("Exercici 2:\na)\n")
+
+print("Valor de x:", x)
+print("Valor de y:", y, "\n")
+
+print("Suma:", x + y)
+print("Resta:", x - y)
+print("Multiplicació:", x * y)
+print("Divisió:", x / y)
+print("Divisió sencera:", x // y)
+print("Resta de la divisió:", x % y)
+
+print("")
+input("Polsa intro per continuar.")
+
+# b)
+
+print("b)")
+
+x = int(input("Posa-li un valor a x:\n"))
+y = int(input("Posa-li un valor a y:\n"))
+
+print("Valor de x:", x)
+print("Valor de y:", y, "\n")
+
+print("Suma:", x + y)
+print("Resta:", x - y)
+print("Multiplicació:", x * y)
+print("Divisió:", x / y)
+print("Divisió sencera:", x // y)
+print("Resta de la divisió:", x % y)
+
+print("")
+input("Polsa intro per continuar.")
+
+# exercici 3
+
+print("Exercici 3:\n")
